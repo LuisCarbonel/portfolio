450c151dfb1897c9580cc05f65a1a1871667df3b
bingi <luis.carbonel.gk@hotmail.com>
2019-08-27 19:49:51 -0400

    navigation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom'
 import './App.css';
 
 class App extends React.Component {
---
+++ portfolio/src/components/header/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Navigation extends React.Component{
+  constructor() {
+    super();
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        Nav bar here...
+      </div>
+    )
+  }
+
+}
